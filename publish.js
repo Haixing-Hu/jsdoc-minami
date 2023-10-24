5a784356afac61cdf540d70fe3a50671231be0ed
Haixing Hu <starfish.hu@gmail.com>
2023-10-24 17:25:05 +0800

    docs: change the style of copyright comments

--- publish.js
+++ publish.js
@@ -1,3 +1,12 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+//    Copyright (c) 2022 - 2023.
+//    Haixing Hu, Qubit Co. Ltd.
+//
+//    All rights reserved.
+//
+////////////////////////////////////////////////////////////////////////////////
+
 /*global env: true */
 "use strict"
 
@@ -399,7 +408,7 @@ function buildMemberNav(items, itemHeading, itemsSeen, linktoFn) {
         nav.push(buildNavItem(linkfoFn('', item.name)))
         return
       }
-      
+
       if (!hasOwnProp.call(itemsSeen, item.longname)) {
         if (!!conf.default.useLongnameInNav) {
           displayName = item.longname
@@ -450,9 +459,9 @@ function linktoExternal(longName, name) {
 }
 
 /**
- * Helper to generate navigation list link wrapper around navigation links for 
+ * Helper to generate navigation list link wrapper around navigation links for
  * locations.
- * 
+ *
  * @param {String} linkClass navigation link classname
  * @param {String} linkContent navigation link HTML content
  * @return {String}
@@ -468,7 +477,7 @@ function buildNavLink (linkClass, linkContent) {
 /**
  * Helper to generate navigation list header wrapper around navigation header content
  * for headings and filenames.
- * 
+ *
  * @param {String} content navigation header content
  * @return {String}
  */
@@ -481,9 +490,9 @@ function buildNavHeading (content) {
 }
 
 /**
- * Helper for generating generic navigation wrapper around content passed for 
+ * Helper for generating generic navigation wrapper around content passed for
  * methods, and types.
- * 
+ *
  * @param {String} itemContent navigation item content
  * @return {String}
  */
